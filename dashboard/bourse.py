17b62a10bd477df08bcf86575f7b6189f2179f9f
Clovis Lechien <clovis.lechien@epita.fr>
2024-05-01 16:27:25 +0200

    feat(dash): finished v2.1

--- dashboard/bourse.py
+++ dashboard/bourse.py
@@ -42,7 +42,7 @@ def run_query(n_clicks, query):
 
 # * -={#|#}=- * -={#|#}=- * -={#|#}=- * TESTING VALUES * -={#|#}=- * -={#|#}=- * -={#|#}=- * #
 
-dates = pd.date_range(start='2022-01-01', end='2022-12-31', freq='h')
+"""dates = pd.date_range(start='2022-01-01', end='2022-12-31', freq='h')
 num_data_points_per_day = 12
 num_days = len(dates) // num_data_points_per_day
 
@@ -64,7 +64,7 @@ for stock in ['Stock_A', 'Stock_B', 'Stock_C', 'Stock_D']:
 
 stock_data = stock_data.drop_duplicates(subset='Date', keep='first')
 
-pure_stock_columns = [col for col in stock_data.columns if '.' not in col]
+pure_stock_columns = [col for col in stock_data.columns if '.' not in col]"""
 
 
 # * -={#|#}=- * -={#|#}=- * -={#|#}=- * APP SETUP * -={#|#}=- * -={#|#}=- * -={#|#}=- * #
@@ -86,15 +86,14 @@ df_companies = df_companies[['id', 'name', 'mid', 'symbol']].copy()
 df_daystocks.rename(columns={"cid": "id", "date": "date_daystocks", "volume": "volume_daystocks"}, inplace=True)
 df_stocks.rename(columns={"cid": "id", "date": "date_stocks", "volume": "volume_stocks"}, inplace=True)
 
-df = df_stocks.merge(df_companies, how='left', on='id').copy()
-df = df.merge(df_daystocks, how='left', on='id').copy()
-df = df[['id', 'name', 'mid',
+# df = df_stocks.merge(df_companies, how='left', on='id').copy()
+# df = df.merge(df_daystocks, how='left', on='id').copy()
+"""df = df[['id', 'name', 'mid',
          'symbol', 'date_stocks', 'value',
          'volume_stocks', 'date_daystocks',
          'open', 'close', 'high', 'low',
-         'volume_daystocks']].copy()  # rename to stock_data
-
-print(df)
+         'volume_daystocks']].copy()  # rename to stock_data"""
+# print(df)
 
 ext = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__,  title="Bourse", suppress_callback_exceptions=True, external_stylesheets=ext)
@@ -110,6 +109,271 @@ colors = {
     'accent': '#FFC0CB',
     'border': '#DDDDDD'
 }
+
+# Selectors
+stock_selector = html.Div(children=[
+    html.Label('Select stocks:', style={'font-weight': 'bold', 'color': colors['text']}),
+    dcc.Dropdown(
+        id='stock-selector',
+        options=[{'label': row['symbol'], 'value': row['symbol']} for index, row in df_companies.iterrows()],
+        value=[df_companies.iloc[0]['symbol']],
+        multi=True,
+        style={'width': '100%', 'background-color': colors['accent'],
+                'box-shadow': '0px 0px 10px rgba(0, 0, 0, 0.1)', 'color': colors['text']},
+    )
+], style={'width': '45%'})
+
+
+graph_selector = html.Div(children=[
+    dcc.Dropdown(
+        id='visualization-type',
+        options=[
+            {'label': 'Lines', 'value': 'lines'},
+            {'label': 'Candlesticks', 'value': 'candlesticks'}
+        ],
+        value='lines',  # valeur par défaut
+        style={'background-color': colors['accent'], 'color': colors['text']}
+    )
+])
+
+# Switches
+bollinger_switch = html.Div(children=[
+    dcc.Checklist(
+        id='bollinger-switch',
+        options=[
+            {'label': 'Show Bollinger Bands', 'value': 'bollinger'}
+        ],
+        value=[],
+        style={'margin-top': '10px', 'color': colors['text']}
+    )
+])
+
+trix_indicator_switch = html.Div(children=[
+    dcc.Checklist(
+        id='trix-indicator-switch',
+        options=[
+            {'label': 'Show TRIX Indicator', 'value': 'trix'}
+        ],
+        value=[],
+        style={'margin-top': '10px', 'color': colors['text']}
+    )
+])
+
+# Date picker
+date_picker = html.Div(children=[
+    html.Label('Select date range:', style={'font-weight': 'bold', 'color': colors['text']}),
+    dcc.DatePickerRange(
+        id='date-picker-range',
+        min_date_allowed=min(df_daystocks['date_daystocks']),
+        max_date_allowed=max(df_daystocks['date_daystocks']),
+        start_date=min(df_daystocks['date_daystocks']),
+        end_date=max(df_daystocks['date_daystocks']),
+        display_format='YYYY-MM-DD',
+        style={'width': '100%', 'color': colors['accent']}
+    )
+], style={'overflow': 'hidden', 'flex': 1, 'margin-left': 'auto'})
+
+
+stock_info_table = dash_table.DataTable(
+    id='stock-table',
+    columns=[
+        {'name': 'Date', 'id': 'date-column'},
+        {'name': 'Min', 'id': 'min-column'},
+        {'name': 'Max', 'id': 'max-column'},
+        {'name': 'Start', 'id': 'start-column'},
+        {'name': 'End', 'id': 'end-column'},
+        {'name': 'Mean', 'id': 'mean-column'},
+        {'name': 'Std Dev', 'id': 'std-dev-column'}
+    ],
+    page_size=10,
+    data=[],
+    style_header={'text-align': 'center', 'background-color': colors['accent']},
+)
+
+# * -={#|#}=- * -={#|#}=- * -={#|#}=- * APP LAYOUT * -={#|#}=- * -={#|#}=- * -={#|#}=- * #
+
+app.layout = html.Div(children=[
+    # Titre
+    html.H1("Stock Dashboard", style={'text-align': 'center', 'color': colors['text'], 'margin': '20px'}),
+
+    html.Div(children=[
+        html.Div(stock_selector, style={'flex': '1'}),
+        html.Div(date_picker, style={'flex': '1', 'margin-left': '20px'}),
+        ],
+        style={'height': '100px', 'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center',
+            'padding-left': '30px', 'background-color': colors['background']}),
+
+    # Div Basse
+    html.Div(
+        children=[
+            # Div Gauche (Graph)
+            html.Div(
+                children=[
+                    dcc.Graph(id='stock-graph'),
+                ],
+                style={'flex': '70%', 'padding': '20px', 'background-color': '#f9f9f9', 'border-radius': '10px',
+                       'box-shadow': '0px 0px 20px rgba(0, 0, 0, 0.3)', 'width': '70%', 'margin-right': '10px'}
+            ),
+
+            # Div Droite (Settings)
+            html.Div(
+                children=[
+                    html.H4("Graph Settings", style={'color': colors['text'],
+                                                        'margin-bottom': '10px'}),
+                    html.Label("Select visualization type:", style={'font-weight': 'bold', 'color': colors['text']}),
+                    graph_selector,
+                    bollinger_switch,
+                    trix_indicator_switch,
+                ],
+                style={'flex': '30%', 'padding': '20px', 'background-color': colors['background'],
+                       'border-radius': '10px',
+                       'box-shadow': '0px 0px 20px rgba(0, 0, 0, 0.3)', 'width': '30%', 'margin-left': '10px'}
+            ),
+        ],
+        style={'display': 'flex', 'justify-content': 'space-between', 'align-items': 'stretch',
+               'margin': '20px', 'background-color': colors['background'], 'padding': '20px'}
+    ),
+
+    # Div Tableau
+    html.Div(
+        children=[
+            html.Label("Info on selected stocks", style={'fontWeight': 'bold', 'color': colors['text']}),
+            stock_info_table,
+        ],
+        style={'margin': '30px', 'padding': '30px', 'background-color': colors['background'], 'border-radius': '10px',
+               'box-shadow': '0px 0px 20px rgba(0, 0, 0, 0.3)'}
+    )
+],
+    style={'background-color': colors['background'], 'font-family': 'Arial, sans-serif', 'color': colors['text'],
+           'border-radius': '15px', 'box-shadow': '0px 0px 20px rgba(0, 0, 0, 0.3)', 'margin': '20px',
+           'padding': '20px'}
+)
+
+# * -={#|#}=- * -={#|#}=- * -={#|#}=- * CALLBACKS * -={#|#}=- * -={#|#}=- * -={#|#}=- * #
+
+
+@callback(
+    Output('stock-graph', 'figure'),
+    Output('stock-table', 'data'),
+    Input('stock-selector', 'value'),
+    Input('visualization-type', 'value'),
+    Input('bollinger-switch', 'value'),
+    Input('trix-indicator-switch', 'value'),
+    Input('date-picker-range', 'start_date'),
+    Input('date-picker-range', 'end_date')
+)
+def update_graph(selected_stocks, visualization_type, bollinger_switch_value, trix_switch_value, start_date, end_date):
+    traces = []
+    data = []
+
+    filtered_data = df_daystocks[
+        (df_daystocks['date_daystocks'] >= start_date) & (df_daystocks['date_daystocks'] <= end_date)]
+
+    for current_stock in selected_stocks:
+        if visualization_type == 'lines':
+            stock_data = df_stocks[df_stocks['symbol'] == current_stock]
+            traces.append(go.Scatter(x=stock_data['date_stocks'],
+                                     y=stock_data['value'],
+                                     mode='lines',
+                                     name=current_stock))
+
+        elif visualization_type == 'candlesticks':
+            traces.append(go.Candlestick(x=filtered_data['date_daystocks'],
+                                         open=filtered_data['open'],
+                                         high=filtered_data['high'],
+                                         low=filtered_data['low'],
+                                         close=filtered_data['close'],
+                                         name=current_stock))
+
+        # Bollinger bands
+        if 'bollinger' in bollinger_switch_value:
+            stock_data = df_stocks[df_stocks['symbol'] == current_stock]
+            mean = stock_data['value'].rolling(window=20).mean()
+            std = stock_data['value'].rolling(window=20).std()
+
+            upper_band = mean + (2 * std)
+            lower_band = mean - (2 * std)
+
+            # Trace Bollinger bands
+            traces.append(go.Scatter(x=stock_data['date_stocks'],
+                                     y=upper_band,
+                                     mode='lines',
+                                     name=f'{current_stock} Bollinger\'s Upper Band',
+                                     line=dict(color='blue', dash='dash')))
+            traces.append(go.Scatter(x=stock_data['date_stocks'],
+                                     y=mean,
+                                     mode='lines',
+                                     name=f'{current_stock} Bollinger\'s Central Band',
+                                     line=dict(color='black', dash='dash')))
+            traces.append(go.Scatter(x=stock_data['date_stocks'],
+                                     y=lower_band,
+                                     mode='lines',
+                                     name=f'{current_stock} Bollinger\'s Lower Band',
+                                     line=dict(color='red', dash='dash')))
+            # Fill space between upper and lower bands
+            traces.append(go.Scatter(x=stock_data['date_stocks'],
+                                     y=upper_band,
+                                     mode='lines',
+                                     line=dict(color='rgba(0,0,255,0)'),
+                                     showlegend=False))
+            traces.append(go.Scatter(x=stock_data['date_stocks'],
+                                     y=lower_band,
+                                     mode='lines',
+                                     fill='tonexty',
+                                     fillcolor='rgba(0,0,255,0.1)',
+                                     line=dict(color='rgba(0,0,255,0)'),
+                                     name=f'{current_stock} Bollinger\'s Band Area'))
+
+        # TRIX indicator
+        if 'trix' in trix_switch_value:
+            trix_data = filtered_data.copy()
+            trix_data[f'{current_stock}.TRIX'] = calculate_trix(trix_data, current_stock, 14)
+
+            traces.append(go.Scatter(x=trix_data['date_daystocks'],
+                                     y=trix_data[f'{current_stock}.TRIX'],
+                                     mode='lines',
+                                     line=dict(color='green', dash='dot'),
+                                     name=f'{current_stock} TRIX'))
+
+    layout = go.Layout(title='Stock Prices', xaxis=dict(title='Date'), yaxis=dict(title='Price'))
+
+    # Grouped data for table
+    grouped_data = df_daystocks.groupby(pd.Grouper(key='date_daystocks', freq='d'))
+    for date, group_data in grouped_data:
+        if start_date <= date <= end_date:
+            row_data = {
+                'date-column': date.strftime('%Y-%m-%d'),
+                'min-column': group_data[selected_stocks].min().min(),
+                'max-column': group_data[selected_stocks].max().max(),
+                'start-column': group_data[selected_stocks].iloc[0, :].min(),
+                'end-column': group_data[selected_stocks].iloc[-1, :].max(),
+                'mean-column': group_data[selected_stocks].mean().mean(),
+                'std-dev-column': group_data[selected_stocks].std().mean()
+            }
+            data.append(row_data)
+
+    fig = {'data': traces, 'layout': layout}
+
+    return fig, data
+
+
+def calculate_trix(data, current_stock, period):
+    # Exponential Moving Average (EMA) of close prices
+    ema_close = data[data['symbol'] == current_stock]['close'].ewm(span=period, min_periods=period).mean()
+
+    # Rate of change of EMA of close prices
+    roc_ema_close = ema_close.pct_change()
+
+    # Triple Exponential Moving Average (TRIX)
+    trix = roc_ema_close.ewm(span=period, min_periods=period).mean() * 100
+
+    return trix
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+"""
 # Selectors
 stock_selector = html.Div(children=[
     html.Label('Select stocks:', style={'font-weight': 'bold', 'color': colors['text']}),
@@ -363,3 +627,4 @@ def calculate_trix(data, current_stock, period):
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
