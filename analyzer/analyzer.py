542195e3eee4bc654fa78003f59e68b8dc95b183
sami.carret <sami.carret@epita.fr>
2024-05-01 17:59:11 +0200

    fix: analyzer daystocks

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -12,10 +12,13 @@ db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # 
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
 
 def last_to_float(last):
-    return float(last.split('(c)')[0].replace(' ', ''))
+    if isinstance(last, float):
+        return last
+    return float(last.split('(')[0].replace(' ', ''))
 
-def get_cid(symbol):
-    return db.execute(query="SELECT id FROM companies WHERE symbol = %s", args=(symbol,))[0][0]
+def get_cid(symbol, companies):
+    cid = companies.index[companies['symbol'] == symbol].tolist()[0]
+    return cid
 
 def store_companies(df, name_bourse, old_companiz=pd.DataFrame()):
     companiz = (
@@ -44,10 +47,14 @@ def store_companies(df, name_bourse, old_companiz=pd.DataFrame()):
     filtered_companiz = companiz[~companiz['symbol'].isin(old_companiz['symbol'])]
     old_companiz = pd.concat([old_companiz, filtered_companiz], ignore_index=True)
     
+    old_companiz.reset_index(drop=True, inplace=True)
+    old_companiz['id'] = old_companiz.index
+    old_companiz.set_index('id', inplace=True)
+    
     return old_companiz
     
 
-def store_stocks(df, timestamp_value, old_stockz=pd.DataFrame()):
+def store_stocks(df, timestamp_value, companies, old_stockz=pd.DataFrame()):
     stockz = (
         df
         .loc[(df['last'] != 0) & (df['volume'] != 0)]
@@ -59,7 +66,7 @@ def store_stocks(df, timestamp_value, old_stockz=pd.DataFrame()):
         
     stockz['date'] = timestamp_value
     stockz['cid'] = 0
-    stockz['cid'] = stockz['symbol'].apply(get_cid)
+    stockz['cid'] = stockz['symbol'].apply(lambda x: get_cid(x, companies))
     stockz.drop(columns=['symbol'], inplace = True)
         
     stockz.set_index('date', inplace=True)
@@ -76,8 +83,11 @@ def push_companies(companiz):
     
 def push_stocks(stockz):
     db.df_write(stockz, "stocks", chunksize=100000, index=True, commit=True)
+    
+def push_daystocks(daystockz):
+    db.df_write(daystockz, "daystocks", chunksize=100000, index=True, commit=True)
 
-def store_file(name, website):
+def store_file(name, website, companiz, stockz):
     #if db.is_file_done():#name):
     #    return
     if website.lower() == "boursorama":
@@ -97,36 +107,64 @@ def store_file(name, website):
         
         df['last'] = df['last'].apply(last_to_float)
         
-        def tempfunc(tp):
-            return tp.split('N')[0]
-        
         # =====================[ MARKET ]=====================
         
         #dfshit = df.copy()
         #dfshit['symbol'] = dfshit['symbol'].apply(tempfunc)
-        companiz = store_companies(df, name_bourse)
+        companiz = store_companies(df, name_bourse, companiz)
         #companiz = store_companies(dfshit, name_bourse, companiz)
         
-        print(companiz)
-        push_companies(companiz)
+        #print(companiz)
+        #push_companies(companiz)
         
         # =====================[ STOCKS ]=====================
         
-        stockz = store_stocks(df, timestamp_value)
+        stockz = store_stocks(df, timestamp_value, companiz.copy(), stockz)
         #print(stockz)
-        push_stocks(stockz)
+        #push_stocks(stockz)
         
         # ====================[ FILE_DONE ]====================
         
-        db.execute(query="INSERT INTO file_done VALUES (%s);", args=(name,), commit=True)
+        #db.execute(query="INSERT INTO file_done VALUES (%s);", args=(name,), commit=True)
         
-def store_day(market, day):
+        return companiz, stockz
+
+def resample_group(df):
+        return df.resample('D').agg({
+            'value': [('open', 'first'), ('close', 'last'), ('high', 'max'), ('low', 'min')],
+            'volume': 'max'
+        })
+
+def store_daystocks(stockz):
+    daystockz = stockz.groupby('cid').apply(resample_group, include_groups=False).dropna()
+    daystockz = daystockz.reset_index()
+    daystockz.columns = ['cid', 'date', 'open', 'close', 'high', 'low', 'volume']
+    
+    daystockz.reset_index(drop=True, inplace=True)
+    daystockz.set_index('date', inplace=True)
+    return daystockz
+
+def store_day(market, day, website):
     files = os.listdir('/home/bourse/data/boursorama/2020')
     files_to_store = [file for file in files if file.startswith(market + " " + day)]
+    
+    companiz = pd.DataFrame()
+    stockz = pd.DataFrame()
+    
+    for fts in files_to_store:
+        companiz, stockz = store_file(fts, website, companiz, stockz)
+    
+    #companiz, stockz = store_file(files_to_store[0], website, companiz, stockz)
+    
+    daystockz = store_daystocks(stockz)
+
+    push_stocks(stockz)
+    push_companies(companiz)
+    push_daystocks(daystockz)
 
 if __name__ == '__main__':
     print(__file__)
-    print(store_day('amsterdam', '2020-01-01'))
+    store_day('amsterdam', '2020-08-05', 'boursorama')
     #store_file("amsterdam 2020-01-01 13_12_01.528372.bz2", "boursorama")
     #store_file("compA 2020-01-01 09:02:02.532411", "boursorama")
     print("Done")
